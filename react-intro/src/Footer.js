7245a0d6ef7b3605bd8093197616b9615dd1492e
Jawid <jawidforotan100@gmail.com>
2022-04-11 10:52:00 +0200

    Footer component is created.

---
+++ react-intro/src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Footer = (props) => {
+  return (
+    <footer>
+      <p>Powered by REACT</p>
+    </footer>
+  );
+};
+
+Footer.propTypes = {};
+
+export default Footer;
