b28a27175a7b4777583b193dfed30fc488d598ed
Jawid <jawidforotan100@gmail.com>
2022-04-11 10:48:44 +0200

    Header componet is created.

---
+++ react-intro/src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Header = (props) => {
+  return (
+    <div>
+      <nav>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Info</a>
+      </nav>
+    </div>
+  );
+};
+
+Header.propTypes = {};
+
+export default Header;
